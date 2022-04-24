7f73b54ee2d29d68e9971fa364faf685b8a37132
Malthaeldai <malthaeldai@keyforge.de>
2022-04-24 14:10:17 +0200

    SSH-Keys via ZSH

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 sudo apt update;
 sudo apt upgrade -y;
-sudo apt install gcc make mc curl wget git gnupg2 apt-transport-https ca-certificates curl zsh python3-pip -y;
+sudo apt install gcc make mc curl wget git gnupg2 apt-transport-https ca-certificates curl zsh python3-pip bindfs -y;
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 wget -q https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb
@@ -26,8 +26,7 @@ sudo usermod --shell /usr/bin/zsh $USER
 echo "SSH-Keys connection"
 read -p 'Windows-Username: ' uservar
 mkdir -p ~/.ssh
-sudo bindfs -u $USER -g $USER -p 0600:ug+D /mnt/c/Users/matth/.ssh/ .ssh/
-
+echo "sudo bindfs -u $USER -g $USER -p 0600:ug+D /mnt/c/Users/$uservar/.ssh/ .ssh/" >> ~/.zshrc
 echo "--------------------------------------------------";
 echo "Pls. re-log to Distro";
 echo "--------------------------------------------------";
