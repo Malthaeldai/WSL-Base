ac91f6aefeac63e2e9683484adc52ab2411039f3
Malthaeldai <matthias.tolle@gmx.de>
2022-10-15 21:35:28 +0200

    Small changes

--- setup-Ubuntu-22.04.sh
+++ setup-Ubuntu-22.04.sh
@@ -1,16 +1,13 @@
 sudo apt update;
 sudo apt upgrade -y;
 sudo apt install gcc make mc curl wget git gnupg2 apt-transport-https ca-certificates curl zsh python3-pip bindfs sshpass -y;
-
 curl https://apt.releases.hashicorp.com/gpg | gpg --dearmor > hashicorp.gpg
 sudo install -o root -g root -m 644 hashicorp.gpg /etc/apt/trusted.gpg.d/
 sudo apt-add-repository "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com focal main"
 sudo dpkg -i libssl1.1_1.1.1f-1ubuntu2.12_amd64.deb 
-
 echo "deb [arch=amd64,armhf,arm64 signed-by=/usr/share/keyrings/powershell.gpg] \
 https://packages.microsoft.com/ubuntu/20.04/prod focal main" | sudo tee /etc/apt/sources.list.d/powershell.list
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /usr/share/keyrings/powershell.gpg >/dev/null
-
 sudo apt update;
 sudo apt install terraform packer powershell -y;
 sudo python3 -m pip install ansible;
