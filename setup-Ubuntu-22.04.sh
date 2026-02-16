4f3ba7bb2c52f80e8fe9a10a4cecf051adb973b9
Matthias Tolle <mtolle@erc-online.de>
2026-02-16 15:35:10 +0100

    StarShip added - Posh removed

--- setup-Ubuntu-22.04.sh
+++ setup-Ubuntu-22.04.sh
@@ -1,6 +1,6 @@
 sudo apt update;
 sudo apt upgrade -y;
-sudo apt install gcc make mc curl wget git gnupg2 apt-transport-https ca-certificates curl zsh python3-pip bindfs sshpass -y;
+sudo apt install gcc make mc curl wget git gnupg2 apt-transport-https ca-certificates curl zsh python3-pip bindfs sshpass zoxide eza bat xsel -y;
 curl https://apt.releases.hashicorp.com/gpg | gpg --dearmor > hashicorp.gpg
 sudo install -o root -g root -m 644 hashicorp.gpg /etc/apt/trusted.gpg.d/
 sudo apt-add-repository "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com focal main"
@@ -13,24 +13,20 @@ sudo apt install terraform packer powershell -y;
 sudo python3 -m pip install ansible;
 sudo pip3 install pywinrm;
 ansible-galaxy install -r ansible_install_collections.yml;
-sudo cp posh-linux-amd64 /usr/local/bin/oh-my-posh;
-sudo chmod +x /usr/local/bin/oh-my-posh;
+curl -sS https://starship.rs/install.sh | sh
+mkdir -p .config;
+cp .config/starship.toml ~/.config;
 cp zsh.tgz ~;
 cd ~;
 tar -xf zsh.tgz
 rm zsh.tgz
 cd -;
-mkdir ~/shell;
-cp schema.json ~/shell;
-cp ubuntu.omp.json ~/shell;
-cp debian.omp.json ~/shell;
 cp zshrc ~/.zshrc;
 sudo usermod --shell /usr/bin/zsh $USER
 echo "SSH-Keys connection"
 read -p 'Windows-Username: ' uservar
 mkdir -p ~/.ssh
-echo "--------------------------------------------------";
-echo "sudo bindfs -u $USER -g $USER -p 0600:ug+D /mnt/c/Users/$uservar/.ssh/ .ssh/"
+echo "sudo bindfs -u $USER -g $USER -p 0600:ug+D /mnt/c/Users/$uservar/.ssh/ .ssh/" | tee -a .zshrc
 echo "--------------------------------------------------";
 echo "Pls. re-log to Distro";
 echo "--------------------------------------------------";
