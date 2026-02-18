f0b6731017d9b065c7abcf34c665db1d22acaa8a
erc-tolle <mtolle@erc-online.de>
2026-02-18 08:59:31 +0100

    Packer-Terraform-Pwsh (#6)
    * EOL
    
    * EOL
    
    * Fonts
    
    * StarShip added - Posh removed
    
    * Hotfix
    
    * Fix SSH-Link
    
    * Config-Pathing adjusted
    
    * Initial commit
    
    * Adding Terraform-Packer-Pwsh
    
    ---------
    
    Co-authored-by: Malthaeldai <71190664+Malthaeldai@users.noreply.github.com>

--- setup-Ubuntu-24.04.sh
+++ setup-Ubuntu-24.04.sh
@@ -7,6 +7,18 @@ sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible -y
 ansible-galaxy install -r ansible_install_collections.yml;
 
+
+wget -O - https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg;
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(grep -oP '(?<=UBUNTU_CODENAME=).*' /etc/os-release || lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list;
+sudo apt update && sudo apt install terraform packer -y;
+
+source /etc/os-release;
+wget -q https://packages.microsoft.com/config/ubuntu/$VERSION_ID/packages-microsoft-prod.deb;
+sudo dpkg -i packages-microsoft-prod.deb;
+rm packages-microsoft-prod.deb;
+sudo apt-get update;
+sudo apt-get install -y powershell;
+
 curl -sS https://starship.rs/install.sh | sh
 mkdir -p ~/.config;
 cp .config/starship.toml ~/.config/starship.toml;
